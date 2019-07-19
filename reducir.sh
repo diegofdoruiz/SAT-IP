b537dc85859137e932cbd44a35ed99e85102c85a
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2019-07-19 00:02:19 -0500

    Avance script SAT to IP

--- reducir.sh
+++ reducir.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+virtualenv -p python3 env
+
+cd env/bin
+
+chmod +x python
+
+chmod +x python3
+
+cd ..
+
+cd ..
+
 cd Reductor
+
+pwd
+
+chmod +x reductor.py
 
 ../env/bin/python reductor.py
