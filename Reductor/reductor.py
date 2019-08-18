11f9c692e57b5915808cb6721a807ba6a74d16ec
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2019-08-17 20:35:35 -0500

    Comentarios

--- Reductor/reductor.py
+++ Reductor/reductor.py
@@ -8,22 +8,28 @@ class Reductor(object):
 		self.file = None
 
 	def createFile(self, old_file):
+		#Saca el nombre completo del archivo
 		old_file_name = os.path.basename(old_file)
+		#Separa la extensión del nombre del archivo
 		old_file_name = old_file_name.split(".cnf")
+		#Crea el nuevo nombre del archivo
 		new_file_name = old_file_name[0] + ".mzn"
 		print('Writing '+new_file_name+' file')
+		#Crea y abre el nuevo archivo
 		self.file = open('../InstanciasMiniZinc/'+new_file_name, "w")
 		self.file.write("% Instancia MiniZinc\n")
 		self.file.write("% "+new_file_name+"\n")
 		self.file.write("\n")
 
 	def writeVariables(self):
+		#Escribe el  nùmero de variables. Indica que cada variable se encuentra entre cero y uno
 		self.file.write("% SAT variables\n")
 		for var in range(1,self.number_of_variables+1):
 			self.file.write("var 0..1: v"+str(var)+";\n")
 		self.file.write("\n")
 
 	def writeAdditionalVariables(self):
+		#Escribe el  nùmero de variables adicionales. Indica que cada variable se encuentra entre cero y uno
 		self.file.write("% Additional variables for IP\n")
 		for var in range(1,self.number_of_variables+1):
 			self.file.write("var 0..1: n_v"+str(var)+";\n")
@@ -31,18 +37,21 @@ class Reductor(object):
 
 	def writeConstraintsVariables(self):
 		#SAT Constraints 0 <= vi <= 1
+		#Escribe las restricciones para cada variable normal
 		self.file.write("% Constraints Between 0 and 1 for SAT variables\n")
 		for var in range(1,self.number_of_variables+1):
 			self.file.write("constraint v"+str(var)+" >= 0 /\\ v"+str(var)+" <= 1;\n")
 		self.file.write("\n")
 
 		#IP Constraints 0 <= n_vi <= 1
+		#Escribe las restricciones para cada variable adicional
 		self.file.write("% Constraints Between 0 and 1 for IP variables\n")
 		for var in range(1,self.number_of_variables+1):
 			self.file.write("constraint n_v"+str(var)+" >= 0 /\\ n_v"+str(var)+" <= 1;\n")
 		self.file.write("\n")
 
 		# Constraints 1 <= vi + n_vi <= 1
+		#Escribe las restricciones para la suma de las variables y su complemento. Entre cero y uno
 		self.file.write("% Constraints 1 <= vi + n_vi <= 1 all variables\n")
 		for var in range(1,self.number_of_variables+1):
 			self.file.write("constraint 1 <= v"+str(var)+" + n_v"+str(var)+" /\\ v"+str(var)+" + n_v"+str(var)+" <= 1;\n")
@@ -51,17 +60,21 @@ class Reductor(object):
 	def writeOneClauseConstraint(self, line_tokens):
 		self.file.write("constraint ")
 		for i in range(0, len(line_tokens)):
-			if i == len(line_tokens)-1:
+			if i == len(line_tokens)-1: #Final de la linea
 				self.file.write(" >= 1;")
 			else:			
 				if int(line_tokens[i]) > 0:
+					#Cuando es el segundo argumento de la suma (variable normal) no se pone más
 					if i == len(line_tokens)-2:
 						self.file.write("v"+line_tokens[i])
 					else:
+						#Cuando es el primer argumento (variable normal) de la suma, se concatena el signo más
 						self.file.write("v"+line_tokens[i]+" + ")
 				elif int(line_tokens[i]) < 0:
+					#Cuando es el segundo argumento de la suma (complemento) no se pone más
 					if i == len(line_tokens)-2:
 						self.file.write("n_v"+str(abs(int(line_tokens[i]))))
+						#Cuando es el primer argumento (complemento) de la suma, se concatena el signo más
 					else:
 						self.file.write("n_v"+str(abs(int(line_tokens[i])))+" + ")
 		self.file.write("\n")
@@ -79,18 +92,22 @@ class Reductor(object):
 		print('--------------------- SAT to IP ---------------------')
 		print('-----------------------------------------------------\n')
 		print('***** Cargando archivos *****\n')
+		#Lee todos los archivo de un directorio y los guarda en la variable files
 		for r, d, f in os.walk(path):
 			for file in f:
 				if '.cnf' in file:
 					files.append(os.path.join(r, file))
 
+		#Inicia la conversiòn del archivo. Abre cada archivo
 		for old_file in files:
 			# Open the file with read only permit
 			f = open(old_file)
 
 			# use readline() to read the first line 
+			#Lee los archivos linea por linea
 			line = f.readline()
 
+			#No lee la linea de las clausulas
 			init_clauses = False
 			counter = 0
 
@@ -103,9 +120,11 @@ class Reductor(object):
 			    # split line 
 			    tokens = line.split()
 			    # Read number of variables and number of clauses
+			    #Archivo en notacion cnf, donde p son los parámetros
 			    if len(tokens) != 0 and tokens[0] in ("p"):
 			
 			    	# number of variables
+			    	#Asignaciòn del numero de variables y clausulas
 			    	self.number_of_variables = int(tokens[2])
 
 			    	# number of clauses
